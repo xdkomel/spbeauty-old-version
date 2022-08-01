75ee413a4d66defe03113a47113687b5934648ca
Camille <xdghost.kamil@gmail.com>
2022-08-01 08:12:07 +0300

    removed unnecessary styles

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -8,14 +8,13 @@ const WrapperInput = styled.div`
 `;
 
 const Input = styled.input`
-    background-color: #F8FAFC;
     outline: none;
     border-radius: 0px;
     border: solid #152842 1px;
     background-color: #D9E2EF;
     width: 100%;
     &:hover {
-        background-color: #F8FAFC8B;
+        background-color: #D9E2EFB8;
     }
 `;
 
