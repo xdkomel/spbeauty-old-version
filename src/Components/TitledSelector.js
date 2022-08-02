9f9e5b7382771a1d4f08a57b49577f6b6ceaad47
Camille <xdghost.kamil@gmail.com>
2022-08-02 11:04:03 +0300

    Made an arrow for the selector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const Select = styled.select`
-    -moz-appearance:none; /* Firefox */
-    -webkit-appearance:none; /* Safari and Chrome */
-    appearance:none;
+    background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
+    background-position: calc(100% - 0.75rem) center !important;
+    -moz-appearance:none !important;
+    -webkit-appearance: none !important; 
+    appearance: none !important;
     outline: none;
     border-radius: 0px;
     border: solid #152842 1px;
