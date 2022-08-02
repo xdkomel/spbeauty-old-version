e18f4958107c62c3311b0e2196835f7c1f47b104
Camille <xdghost.kamil@gmail.com>
2022-08-02 10:11:11 +0300

    added App.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './typography.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
