35b3dfad114cc66cdc0a77123b637bd1b61938fe
mabramychev <123659070+mabramychev@users.noreply.github.com>
2023-01-26 18:07:45 +0000

    updated old design

--- src/Layouts/Page/Header/Components/IconsRow.js
+++ src/Layouts/Page/Header/Components/IconsRow.js
@@ -5,32 +5,22 @@ import NotificationsIcon from "../../../../Icons/NotificationsIcon";
 import IconOnlyButton from "../../../../Components/IconOnlyButton";
 
 const ButtonLeftWrapper = styled.div`
-    color: #D3EAFE;
     padding-right:6px;
-    &:hover {
-        opacity: 80%;
-    }
 `;
 const ButtonRightWrapper = styled.div`
-    color: #D3EAFE;
     padding-left:6px;
-    &:hover {
-        opacity: 80%;
-    }
 `;
 
 function IconsRow() {
     return (
         <div className='d-inline-flex flex-row align-items-center align-self-center'>
             <ButtonLeftWrapper>
-                <Clickable>
-                    <div className="text-rg-bd">Сообщения (0)</div>
-                </Clickable>
+                <IconOnlyButton icon={<MessagesIcon/>}/>
+
+
             </ButtonLeftWrapper>
             <ButtonRightWrapper>
-                <Clickable>
-                    <div className="text-rg-bd">Оповещения (0)</div>
-                </Clickable>
+                <IconOnlyButton icon={<NotificationsIcon/>}/>
             </ButtonRightWrapper>
         </div>
     );
