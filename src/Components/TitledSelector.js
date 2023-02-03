3e7c245246a94d3ce1c2ad11d224d0d392792df4
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-03 21:39:41 +0000

    title selector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -8,11 +8,9 @@ const Select = styled.select`
     appearance: none !important;
     outline: none;
     border-radius: 24px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
-    &:hover {
-        background-color: #D9E2EFB8;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #0F172A;
     }
 `;
 
