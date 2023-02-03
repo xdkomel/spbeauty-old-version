ad4146c7b2516a448b2d0c542623c66d1109462a
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-03 21:25:48 +0000

    adress title

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4 col'>
-            <div className='text-rg'>Адреса заведений</div>
+            <div className='title'> заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
