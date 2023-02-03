e55636fb1c5df252150b3d5ca20f7ab5307f5efc
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-03 22:57:56 +0000

    ms-4

--- src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
+++ src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
@@ -3,7 +3,7 @@ function Profile() {
     return (
         <div className='d-inline-flex flex-row'>
             <Avatar/>
-            <div className='ms-3 d-inline-flex flex-column justify-content-center'>
+            <div className='ms-4 d-inline-flex flex-column justify-content-center'>
                 <div className='title mb-2'>SPBEAUTY</div>
                 <div className="mb-1">Салон красоты</div>
                 <div>Санкт-Петербург, Невский пр-т. 28</div>
