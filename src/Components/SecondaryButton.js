f5f56e7a3c45e93b565036b7340334cd94fd8f70
aneveline <123659636+aneveline@users.noreply.github.com>
2023-02-03 20:31:46 +0000

    secondary button

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,16 +5,17 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        background: #ffffff;
+        border: 1px solid #64748B;
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #ffffff80;
+            border: 1px solid #64748B80;
         }
         &:active {
-            background: #DFDFDF;
+            background: #ffffff60;
+            border: 1px solid #64748B60;
         }
     `;
     return (
